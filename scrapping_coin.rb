293ff4eeb9e838d3caa7c640b42fdb526956ba4d
yaqizhao <yaqi.zhao29@gmail.com>
2018-10-11 23:49:32 +0200

    cours without name

--- scrapping_coin.rb
+++ scrapping_coin.rb
@@ -6,8 +6,8 @@ def trader_de_lobscur
   array_de_hashs = []
   page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
   page.at_css('#currency-exchange-rates').each do |key, value| 
-    hash = Hash.new(key.to_s)
-    hash[key.to_s] = value.to_f 
+    hash = Hash.new
+    hash["value"] = value.to_f 
     array_de_hashs << hash
   end
   return array_de_hashs
